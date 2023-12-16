bf92ad707b2fc385513ebc9ddc6e1da0d1e54394
blyavova <grushvo3@gmail.com>
2023-12-16 14:49:34 +0200

    update

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
-const use = document.querySelector('#svg')
-const btn = document.querySelector('.wallet-and-instruments-btn')
+const refs = {
+  headerA: document.querySelectorAll('.text-white-js'),
+  cardBtn: document.querySelectorAll('#card-btn'),
+  leftBtn:  document.querySelectorAll('#left'),
+  rightBtn: document.querySelectorAll('#right')
+};
 
-console.log(use , btn)
+console.log(refs);
 
-btn.addEventListener(mouseover, function (a) {
-    use.href = './images/symbol-defs.svg#icon-apple'
-})
+
+Array.from(refs.headerA).forEach((element) => {
+  element.addEventListener("mouseover", (event) => {
+    element.style.color = "white";
+  });
+
+  element.addEventListener("mouseout", (event) => {
+    element.style.color = ""; 
+  });
+});
+
+Array.from(refs.cardBtn).forEach((element) => {
+  element.addEventListener("click", (event) => {
+    element.style.background = "#5653FF";
+    setTimeout(() => {
+      element.style.backgroundColor = "";
+    }, 500);
+  });
+});
